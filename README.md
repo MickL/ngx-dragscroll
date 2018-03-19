ffb3c7c13198aa5adf71311b6552e1f6cf2d778e
Mick Lawitzke <Info@Lawitzke.de>
2018-03-19 20:07:15 +0100

    Directive should run outside outside of Angular

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Angular directive to scroll on drag / mousedown
 
 Requires at least Angular 5+ (RxJS 5.5+)
 
-Thanks to (Ben Lash)[https://youtu.be/3LKMwkuK0ZE?t=3m14s] and (asvd/dragscroll)[https://github.com/asvd/dragscroll]
+Thanks to [Ben Lash](https://youtu.be/3LKMwkuK0ZE?t=3m14s) and [asvd/dragscroll](https://github.com/asvd/dragscroll)
--- ngx-dragscroll.ts
+++ ngx-dragscroll.ts
@@ -1,13 +1,14 @@
-import {Directive, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Directive, ElementRef, Input, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {fromEvent} from 'rxjs/observable/fromEvent';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import {switchMap, takeUntil} from 'rxjs/operators';
 
 @Directive({
-  selector: '[dragScroll]'
+  selector: '[appDragScroll]'
 })
 export class DragScrollDirective implements OnInit, OnDestroy {
+  @Input() dragScrollOnMobile = true;
   mousedown$: Observable<MouseEvent>;
   mousemove$: Observable<MouseEvent>;
   mouseup$: Observable<MouseEvent>;
@@ -16,11 +17,10 @@ export class DragScrollDirective implements OnInit, OnDestroy {
   lastX: number;
   lastY: number;
 
-  constructor(private el: ElementRef) {
-    console.log(el.nativeElement);
+  constructor(private el: ElementRef, private ngZone: NgZone) {
   }
 
-  ngOnInit() {
+  init() {
     // Create observables from mouse events
     this.mousedown$ = fromEvent(this.el.nativeElement, 'mousedown');
     this.mouseup$ = fromEvent(document, 'mouseup');
@@ -44,7 +44,14 @@ export class DragScrollDirective implements OnInit, OnDestroy {
       .subscribe(event => {
         this.el.nativeElement.scrollLeft -= (-this.lastX + (this.lastX = event.clientX));
         this.el.nativeElement.scrollTop -= (-this.lastY + (this.lastY = event.clientY));
-      });
+      })
+    ;
+  }
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.init();
+    });
   }
 
   ngOnDestroy() {
